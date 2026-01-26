10adcca8ad93ad52630e114791fdeafe189e1d2f
mrycdng <163579194+mrycdng@users.noreply.github.com>
2026-01-26 11:32:10 -0500

    css

---
+++ script.js
@@ -0,0 +1,7 @@
+// const MIN = -1;
+//   const MAX = 1;
+
+//   document.querySelectorAll("p").forEach(p => {
+//     const angle = Math.random() * (MAX - MIN) + MIN;
+//     p.style.transform = `rotate(${angle}deg)`;
+//   });
