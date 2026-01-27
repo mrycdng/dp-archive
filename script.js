33023b0ad9a12bf32e963a6917a95990cb9b3d78
mrycdng <163579194+mrycdng@users.noreply.github.com>
2026-01-26 23:56:04 -0500

    css

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-// const MIN = -1;
-//   const MAX = 1;
+const MIN = -1;
+const MAX = 1;
+// const angle = Math.random() * (MAX - MIN) + MIN;
+
+// const smallcaps = document.getElementsByClassNmae("small-caps");
 
 //   document.querySelectorAll("p").forEach(p => {
 //     const angle = Math.random() * (MAX - MIN) + MIN;
 //     p.style.transform = `rotate(${angle}deg)`;
 //   });
+
+// smallcaps.style.transform = `rotate(${angle}deg)`;
