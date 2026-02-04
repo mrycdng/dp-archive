26a4319995c412c676f3ec10dadebfdb36e3ab24
mrycdng <163579194+mrycdng@users.noreply.github.com>
2026-02-03 23:33:27 -0500

    fixed homepage about overlay bug

---
+++ home-about.js
@@ -0,0 +1,26 @@
+//toggle about on and off
+
+const about = document.getElementById('about');
+const plus = document.getElementById('plus');
+
+let clicked = true;
+
+let showOrHide = function(){
+    if (clicked) {
+        about.style.display = 'block';
+
+        plus.innerHTML = "X";
+        plus.style.mixBlendMode = "multiply";
+        plus.style.backgroundColor = "white";
+
+        clicked = false;
+    }
+    else {
+        about.style.display = 'none';
+
+        plus.innerHTML = "+";
+        plus.style.mixBlendMode = "screen";
+
+        clicked = true;
+    }
+}
