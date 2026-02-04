45ad5f512b8ed2349a0edf2765789410f3aca614
mrycdng <163579194+mrycdng@users.noreply.github.com>
2026-02-03 23:26:34 -0500

    updated h1 + home button

--- about.js
+++ about.js
@@ -2,20 +2,30 @@
 
 const about = document.getElementById('about');
 const plus = document.getElementById('plus');
+const home = document.getElementById('home');
+
 let clicked = true;
 
 let showOrHide = function(){
     if (clicked) {
         about.style.display = 'block';
+
         plus.innerHTML = "X";
         plus.style.mixBlendMode = "multiply";
         plus.style.backgroundColor = "white";
+
+        home.style.display = "none";
+
         clicked = false;
     }
     else {
         about.style.display = 'none';
+
         plus.innerHTML = "+";
         plus.style.mixBlendMode = "screen";
+
+        home.style.display = "block";
+
         clicked = true;
     }
 }
